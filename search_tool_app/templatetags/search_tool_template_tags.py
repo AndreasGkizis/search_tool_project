dc15f5fbe3ca6d35ebbf0a1a2976fca3872feec6
Andreas <andreasgkizis@gmail.com>
2021-03-11 15:14:47 +0200

    edit homepage, register active tab tag(needs fixing)

---
+++ search_tool_app/templatetags/search_tool_template_tags.py
@@ -0,0 +1,13 @@
+from django import template
+from django.urls import reverse
+
+register = template.Library()
+
+
+@register.simple_tag
+def is_active(request, url):
+    # if on the current url the return "active" otherwise return ""
+    if request.path in reverse(url):
+        return "active"
+    else:
+        return ""
