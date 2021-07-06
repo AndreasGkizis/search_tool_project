700a4ec4b51ad1a33dfe38b44b579240c2353514
Andreas <andreasgkizis@gmail.com>
2021-07-06 23:06:18 +0300

    search filters working

--- search_tool_app/views.py
+++ search_tool_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .forms import PaperForm, SearchForm
 from .models import Language, Year, Type, Tag, Paper
+from functools import reduce
+from django.db.models import Q
 from django.urls import reverse
 
 
@@ -26,7 +28,7 @@ def post_a_publication(request):
 
 
 def search_publication(request):
-    # import ipdb;ipdb.set_trace()
+    # import ipdb; ipdb.set_trace()
 
     form = SearchForm()
     if request.method == 'GET':
@@ -38,37 +40,56 @@ def search_publication(request):
     elif request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
-            if not form.cleaned_data["type"]:
-                #  maybe this should not be None since it returns an empty list nad not None
-                results = Paper.objects.filter(title__icontains=form.cleaned_data["title"])
+            title_filter = form.cleaned_data['title']
+            year_filter = form.cleaned_data['year'].values_list('id', flat=True)
+            type_filter = form.cleaned_data['type'].values_list('id', flat=True)
+            lang_filter = form.cleaned_data['language'].values_list('id', flat=True)
+            tag_filter = form.cleaned_data['tag'].values_list('id', flat=True)
+            material_filter = form.cleaned_data['material'].values_list('id', flat=True)
+            filters_dict = {}
 
-            else:
-                results = Paper.objects.filter(title__icontains=form.cleaned_data["title"]).filter(
-                                               type_id__type__icontains=form.cleaned_data["type"])
-            return render(request, 'search_tool_app/search_publication.html', {'results': results, 'form': form})
+            if title_filter:
+                filters_dict['title__icontains'] = title_filter
+            if year_filter:
+                filters_dict['year_id__in'] = year_filter
+            if type_filter:
+                filters_dict['type_id__in'] = type_filter
+            if lang_filter:
+                filters_dict['language__in'] = lang_filter
+            if tag_filter:
+                filters_dict['tag__in'] = tag_filter
+            if material_filter:
+                filters_dict['material__in'] = material_filter
+
+            results = Paper.objects.filter(**filters_dict)
+
+            return render(request, 'search_tool_app/search_publication.html', {
+                'results': results,
+                'form': form
+            })
         else:
-            return "firm is not valid"
+            return "form is not valid"
 
-    return render(request, 'search_tool_app/search_publication.html',
-                  context={'form': form}
-                  )
+    return render(request, 'search_tool_app/search_publication.html', {
+                                                                        'form': form
+                                                                                   })
 
 
+# papers = Paper.objects.filter(reduce(lambda x, y: x | y, [Q(type_id__type__icontains=word) for word in
+#                                                  form.cleaned_data["type"].values_list("type", flat
+#                                                  =True)]))    h kagouria tou sergiou pou douleuei
 def show_publication(request, slug):
     # if request.method == "GET":
     obj = Paper.objects.get(slug=slug)
     context = {
         'publications': obj,
         'slug': slug  # ti to ithele ayto den katalabainw
-        }
+    }
     if request.method == "GET":
         result = Paper.objects.filter(slug__contains="slug")
 
-        context={ # Paper obj mallon me slug filter
+        context = {  # Paper obj mallon me slug filter
 
         }
         return render(request, 'search_tool_app/show_publication.html', context)
     return render(request, 'search_tool_app/show_publication.html', context)
-
-
-
