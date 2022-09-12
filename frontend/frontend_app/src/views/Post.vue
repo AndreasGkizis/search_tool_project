f2236be68f2ba097d935c269a25753e33d1f45c5
Andreas <andreasgkizis@gmail.com>
2022-09-12 16:22:33 +0300

    typos fix in Post.vue

--- frontend/frontend_app/src/views/Post.vue
+++ frontend/frontend_app/src/views/Post.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>This page is reserved for the funtinality to add publications to the database</h1>
+        <h1>This page is reserved for the functionality to add publications to the database</h1>
         <h2>To be added soon ....</h2>
     </div>
 </template>
